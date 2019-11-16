b07e53ae68e244cd5e46e736e7ba8ab7c89e288f
johnrice <jrice@labact.com>
2019-11-16 13:08:36 -0500

    Include some more hints

--- models/index.js
+++ models/index.js
@@ -1,7 +1,14 @@
 const Sequelize = require('Sequelize')
-// need to create this:
+
+// @todo - we need to create this folder and sequelize.js file
+//   or using the module 8 slides as reference, complete this config
 const allConfigs = require('../config/sequelize')
 
 // create a connection
+// @todo - using the monday-funday example for a refrence
+//   or using the module 8 slides as reference, complete this connection
+const connection = Sequelize()
 
+// @todo - create the teams.js model file
 // export an instance of the team model
+exports.TeamModel = require('./teams')
